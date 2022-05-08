2c5714998ce0e4efe7982254b1b8c47e9e491294
Kavindu Dilshan <kavindukd1@gmail.com>
2022-05-08 21:33:35 +0530

    Final Project

---
+++ Oder Management/AboutUs.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/aboutus.css" />
+  </head>
+    <body>
+    <section class="about">
+      <div class="main">
+        <img src="food3.jpg" />
+        <div class="about-text">
+          <h1>About Us</h1>
+          <h5>Hungry Hub</h5>
+          <br />
+          <h2>OUR <span>STORY</span></h2>
+          <h3>
+            The Hungry Hub is all about fun and good times with people you care
+            about, sharing original Foods hand-made in the traditional way.
+            Since 2005 we hope you'll try our menu, crafted with care to deliver
+            great value and our signature Loveable pure food Taste. Enjoy the
+            Hungry hub food at home. See you soon!
+          </h3>
+        </div>
+      </div>
+    </section>
+  </body>
+  <div class="footer-v1-static-content">
+    <div class="kc-css-994088 kc_row">
+       <div class="kc-row-container  kc-container">
+          <div class="kc-wrap-columns">
+             <div class="kc-css-194963 kc_col-sm-12 kc_column kc_col-sm-12">
+                <div class="stretch-full-width kc-col-container">
+                   <div class="kc-css-126640 kc_shortcode kc_wrap_instagram  kc_ins_col_6">
+                      
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+ 
+ <!-- #colophon -->
+</div>
+</html>
