b39ef58974def882cb6e6967ecb3c4829c9ec62f
madushankaSamarajeewa <104161826+madushankaSamarajeewa@users.noreply.github.com>
2022-05-08 18:47:18 +0530

    index page

---
+++ employeeManagement/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Employee Management</title>
+
+    <style>
+.center {
+  margin: auto;
+  width: 60%;
+  border: 1px solid #797EF6;
+  padding: 10px;
+}
+</style>
+</head>
+<body>
+    <div class="container">
+            <h1  class='text-center' >Employee Management</h1>
+            
+
+            <div class="center">
+                        <a href="addEmployee.php">
+                            <button type="button" class="btn btn-primary btn-lg btn-block">Add New Employee</button>
+                        </a></br>
+                        <a href="viewEmployeeh.php">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">View Employee Details</button>
+                        </a></br>
+                        <a href="">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">Search Employee</button>
+                        </a></br>
+                        <a href="">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">Generate Reports</button>
+                        </a></br>
+
+            </div>
+
+    </div>
+</body>
+</html>
